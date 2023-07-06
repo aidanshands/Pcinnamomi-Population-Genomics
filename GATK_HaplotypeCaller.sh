b1303fa3586246defb636552bae0fa2d53d5ad7f
Aidan C. Shands <33665233+aidanshands@users.noreply.github.com>
2023-07-05 17:31:04 -0700

    Add files via upload

---
+++ GATK_HaplotypeCaller.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+#SBATCH -N 1
+#SBATCH -n 16
+#SBATCH --mem=32G
+#SBATCH --time=20-00:00:00
+##SBATCH --output=my.stdout
+#SBATCH --job-name="GATK Diploid"
+#SBATCH --array=1-136
+#SBATCH -p batch
+# Load modules
+# working dir: /bigdata/manosalvalab/pmanosal/Pc_PopGen_2022/variant_calling
+module load picard
+module load java/17.0.2
+module load gatk/4.2.5.0
+module load bcftools
+module load samtools/1.14
+
+MEM=32g
+SAMPFILE=Isolate_Names.csv
+
+if [ -f config.txt ]; then
+    source config.txt
+fi
+
+DICT=$(echo $REFGENOME | sed 's/fasta$/dict/')
+
+if [ ! -f $DICT ]; then
+        picard CreateSequenceDictionary R=$GENOMEIDX O=$DICT
+fi
+mkdir -p $GVCFFOLDER
+CPU=1
+if [ $SLURM_CPUS_ON_NODE ]; then
+ CPU=$SLURM_CPUS_ON_NODE
+fi
+N=${SLURM_ARRAY_TASK_ID}
+
+if [ ! $N ]; then
+ N=$1
+fi
+
+if [ ! $N ]; then
+ echo "need to provide a number by --array slurm or on the cmdline"
+ exit
+fi
+
+hostname
+date
+IFS=,
+tail -n +2 $SAMPFILE | sed -n ${N}p | while read STRAIN SAMPID
+do
+  # BEGIN THIS PART IS PROJECT SPECIFIC LIKELY
+  # END THIS PART IS PROJECT SPECIFIC LIKELY
+  echo "STRAIN is $STRAIN"
+  GVCF=$GVCFFOLDER/$STRAIN.g.vcf.gz
+  ALNFILE=$ALNFOLDER/$STRAIN.RG.mkdup.bam
+  if [ -s $GVCF ]; then
+    echo "Skipping $STRAIN - Already called $STRAIN.g.vcf.gz"
+    exit
+  fi
+  if [[ ! -f $GVCF || $ALNFILE -nt $GVCF ]]; then
+      time gatk --java-options -Xmx${MEM} HaplotypeCaller \
+          -ERC GVCF --sample-ploidy 2 \
+          -I $ALNFILE -R $REFGENOME \
+          -O $GVCF --native-pair-hmm-threads $CPU
+ fi
+done
---
+++ VCFtools_Filter.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH--mem=64G
+#SBATCH -c 32
+#SBATCH --time=12-00:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --job-name="LF v3"
+#SBATCH -p batch
+
+module load picard
+module load java/17.0.2
+module load gatk/4.2.5.0
+module load bcftools
+module load samtools/1.14
+module load miniconda3
+module load vcftools/0.1.16-18
+
+#-------------------------------------------------------------------------------
+# Global settings
+# setting dirs
+wd=/bigdata/manosalvalab/pmanosal/Pc_PopGen_2022/variant_calling_diploid
+# setting variables and names
+vcf=Pc_only136.p2.f1SV.vcf.gz
+fil_wd=Light_Filtering
+#-------------------------------------------------------------------------------
+# Identify Singletons
+singletons=$fil_wd/singletons
+singletons_out=$fil_wd/singletons.singletons
+singletons_Pos=$fil_wd/Singletons_Pos.singletons
+# create singletons file and extract position
+CMD="vcftools --gzvcf $vcf --singletons --out $singletons"
+echo $CMD
+#eval $CMD
+awk 'NF{NF-=3};3' < $singletons_out > $singletons_Pos
+#-------------------------------------------------------------------------------
+# Light filtering part 1
+max_missing_vars_1=1 # No missing data
+min_dp=4
+min_Q=20
+remove_sms_out="$fil_wd/""$(basename --suffix ".vcf.gz" $vcf)"".vcft_dp${min_dp}_Q${min_Q}_NoSingleton_noMD"
+CMD="vcftools --gzvcf $vcf --out $remove_sms_out --recode --recode-INFO-all \
+--max-missing $max_missing_vars_1 --minDP $min_dp --minQ $min_Q --exclude-positions $singletons_Pos"
+echo $CMD
+#eval $CMD
+#-------------------------------------------------------------------------------
+# Light filtering part 2
+vcf2=Light_Filtering/Pc_only136.p2.f1SV.vcft_dp4_Q20_NoSingleton_noMD.recode.vcf
+bed=Pc2113T1_genome.repeats.v2.bed
+F2_out="$fil_wd/Pc_only136.p2.f1SV.vcft_dp4_Q20_NoSingleton_noMD_NoRepeats"
+CMD="vcftools --vcf $vcf2 --out $F2_out --recode --recode-INFO-all --exclude-bed $bed"
+echo $CMD
+#eval $CMD
