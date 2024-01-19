e6c2b3bd9fb98e53b35d77fa7e898ff930cd4358
Aidan C. Shands <33665233+aidanshands@users.noreply.github.com>
2024-01-19 11:33:53 -0800

    Update PotassiumPhosphite_Analysis.R

--- PotassiumPhosphite_Phenotyping/PotassiumPhosphite_Analysis.R
+++ PotassiumPhosphite_Phenotyping/PotassiumPhosphite_Analysis.R
@@ -4,7 +4,12 @@ library(ggplot2)
 library(agricolae)
 library(car)
 library(dplyr)
+library(FSA)
+library(tidyverse)
+library(rcompanion)
+library(ggthemes)
 #-------------------------------------------------------------------------------
+setwd("/Users/manosalvalab/Desktop/Aidan/Pc_PopGen_2022/PopGen_Scripts_Github/PotassiumPhosphite_Phenotyping")
 PPP_Data <- read.csv("PPEC50_Data.csv", header = T)
 
 Cols <- c("South" = "#7570B3", "North" = "#D95F02")
@@ -21,10 +26,14 @@ table(PPP_Data$Growing_Region)
 QC_Stats = function(data, variable){
   dataname = deparse(substitute(data))
   col <- data[[variable]]
-  print(paste("Levene's Test for: ", dataname, sep=""))
+  print(paste("Levene's Test by replicate for: ", dataname, sep=""))
   print(leveneTest(col ~ factor(Replicate), data = data))
-  print(paste("Bartlett's Test for: ", dataname, sep=""))
+  print(paste("Levene's Test by Isolate for: ", dataname, sep=""))
+  print(leveneTest(col ~ factor(Isolate), data = data))
+  print(paste("Bartlett's Test by replicate for: ", dataname, sep=""))
   print(bartlett.test(col ~ factor(Replicate), data = data)$p.value)
+  print(paste("Bartlett's Test by Isolate for: ", dataname, sep=""))
+  print(bartlett.test(col ~ factor(Isolate), data = data)$p.value)
   print(paste("Shapiro Wilks test for: ", dataname, sep=""))
   print(shapiro.test(col))
   print(paste("QQplot: ", dataname, sep=""))
@@ -32,7 +41,7 @@ QC_Stats = function(data, variable){
 }
 
 # Performs One-way ANOVA, Tukey's HSD or Fisher's LSD
-Stats = function(data, variable, value, test){
+P_Stats = function(data, variable, value, test){
   dataname = deparse(substitute(data)) # get name of data we are working on 
   print(paste("Stats for ", dataname, sep=""))
   print(paste("One-Way ANOVA for: ", dataname, sep=""))
@@ -43,8 +52,10 @@ Stats = function(data, variable, value, test){
   print(outnameAOV) # print Summary
   if (test == "Fisher"){
     TestResults = (LSD.test(AOV, factor(variable), group=TRUE))# run Fisher's LSD
+    print(TestResults)
   } else if (test == "Tukey"){
     TestResults = (HSD.test(AOV, factor(variable), group=TRUE))# run Tukey HSD
+    print(TestResults)
   }
   # generate a df with Tukey HSD or Fisher's results
   results <- data.frame(
@@ -56,18 +67,22 @@ Stats = function(data, variable, value, test){
   return(results)
 }
 
-# Summarize data (mean+sd)
-detach(package:dplyr, unload=TRUE)
-data_summary <- function(data, varname, groupnames){
-  require(plyr)
-  summary_func <- function(x, col){
-    c(mean = mean(x[[col]], na.rm=TRUE),
-      sd = sd(x[[col]], na.rm=TRUE))
-  }
-  data_sum<-ddply(data, groupnames, .fun=summary_func,
-                  varname)
-  data_sum <- rename(data_sum, c("mean" = varname))
-  return(data_sum)
+# Performs Kruskal-Wallis test and the Dunn's test (with letter groups assigned) (Non-parametric)
+NP_Stats = function(data, variable, value){
+  dataname = deparse(substitute(data)) # get name of data we are working on 
+  print(paste("Stats for ", dataname, sep=""))
+  print(paste("Kruskal-Wallis for: ", dataname, sep=""))
+  # generate a formula (value~variable)
+  formula =reformulate(variable, response = value)
+  KW = kruskal.test(formula, data = data) # run one-way anova
+  print(KW) # print Summary
+  print(paste("Dunn's Test Groups for: ", dataname, sep=""))
+  DTresult = dunnTest(formula, data = data, method="holm")$res
+  DunnResults = cldList(P.adj ~ Comparison, data = DTresult, threshold = 0.05, remove.zero = FALSE)
+  DunnResults = DunnResults %>%
+    select(-MonoLetter) %>%
+    rename(Isolate = Group, Dunn_Group = Letter)
+  return(DunnResults)
 }
 
 #-------------------------------------------------------------------------------
@@ -79,8 +94,9 @@ QC_Stats(PPP_Data, "Log10_EC50")
 #-------------------------------------------------------------------------------
 # One-way ANOVA looking at EC50 by Isolate & Tukey's HSD/Fisher's LSD
 #-------------------------------------------------------------------------------
-IS_TT = Stats(PPP_Data, "Isolate", "EC50", "Tukey")
-IS_FLSD = Stats(PPP_Data, "Isolate", "EC50", "Fisher")
+IS_TT = P_Stats(PPP_Data, "Isolate", "EC50", "Tukey")
+IS_FLSD = P_Stats(PPP_Data, "Isolate", "EC50", "Fisher")
+IS_DUNN = NP_Stats(PPP_Data, "Isolate", "EC50")
 
 #-------------------------------------------------------------------------------
 # Subset CA isolates 
@@ -90,21 +106,34 @@ CA = subset(PPP_Data, State == "CA")
 #-------------------------------------------------------------------------------
 # One-way ANOVA looking at EC50 by Growing Region & Tukey's HSD/Fisher's LSD
 #-------------------------------------------------------------------------------
-IS_TT_CA = Stats(CA, "Growing_Region", "EC50", "Tukey")
-IS_FLSD_CA = Stats(CA, "Growing_Region", "EC50", "Fisher")
+IS_TT_CA = P_Stats(CA, "Growing_Region", "EC50", "Tukey")
+print(IS_TT_CA)
+
+IS_FLSD_CA = P_Stats(CA, "Growing_Region", "EC50", "Fisher")
+print(IS_FLSD_CA)
+print(kruskal.test(EC50 ~Growing_Region, data = CA)) # Significant p<0.05
 
 #-------------------------------------------------------------------------------
 # Summarize Data
 #-------------------------------------------------------------------------------
-PPP_SUM <- data_summary(PPP_Data, varname="EC50", 
-                        groupnames=c("Isolate", "County", "State", "Country", "Growing_Region"))
+PPP_SUM <- as.data.frame(PPP_Data %>% group_by(Isolate, County, State, Country, Growing_Region) %>% 
+                         dplyr::summarize(Mean = mean(EC50, na.rm=TRUE), SD = sd(EC50, na.rm=TRUE)))
 
-# joining TT groups for each isolate to PPP_SUM
+PPP_SUM = PPP_SUM %>% mutate(across(where(is.numeric), ~ round(., 2)))
+
+# joining TT, FLSD & Dunn groups for each isolate to PPP_SUM
 TT_j = subset(IS_TT, select = c(Isolate, Groups))
 colnames(TT_j) <- c('Isolate', 'TT_Groups')
-Sum_List = list(PPP_SUM, TT_j)
+FLSD_j = subset(IS_FLSD, select = c(Isolate, Groups))
+colnames(FLSD_j) <- c('Isolate', 'FLSD_Groups')
+DUNN_j = subset(IS_DUNN, select = c(Isolate, Dunn_Group))
+colnames(DUNN_j) <- c('Isolate', 'Dunn_Groups')
+
+Sum_List = list(PPP_SUM, TT_j, FLSD_j, DUNN_j)
 # Summary used in Table S4
 Comb_Summary = Reduce(function(x, y) merge(x, y, all=TRUE), Sum_List) 
+colnames(Comb_Summary) <- c('Isolate', 'County', 'State',  'Country', 'Growing_Region',
+                            "Mean_EC50", 'SD', 'TT_Groups', 'FLSD_Groups', 'Dunn_Groups')
 #write.csv(Comb_Summary, file = "EC50_Sum.csv")
 
 #-------------------------------------------------------------------------------
@@ -112,38 +141,38 @@ Comb_Summary = Reduce(function(x, y) merge(x, y, all=TRUE), Sum_List)
 #-------------------------------------------------------------------------------
 CA_SUM = subset(Comb_Summary, State == "CA")
 table(CA_SUM$Growing_Region)
-
 #-------------------------------------------------------------------------------
 # Visualizations
 #-------------------------------------------------------------------------------
 #-----------
 # Figure 3C
 #-----------
-BP4 = ggplot(CA_SUM, aes(x=Growing_Region, y=EC50, fill= Growing_Region)) + geom_boxplot()
-BP4= BP4 + theme_classic() + scale_fill_manual(values=Cols) + 
-  labs(x="Growing Region", y = "EC50")+
+BP4 = ggplot(CA_SUM, aes(x=Growing_Region, y=Mean_EC50, fill= Growing_Region)) + geom_boxplot()
+BP4= BP4 + theme_few() + scale_fill_manual(values=Cols) + 
+  labs(x="Growing Region", y = expression(EC[50] ~ paste("(",mu, "g/ml)")))+
   theme(axis.text.x = element_text(size = 8),
         axis.text.y = element_text(size = 8),
-        axis.title=element_text(size=10,face="bold"),
+        axis.title=element_text(size=10),
         legend.position = "none")
 BP4
-#ggsave("PPP_EC50_Boxplot_Region.pdf",BP4, width=6, height=4, units="in")
+ggsave("PPP_EC50_Boxplot_Region_Paper.v4.pdf",BP4, width=4.28, height=3.78, units="in")
 
+# Since the Kruskal-wallis test was significant, I'm placing a, and b above the 
+# boxes to indicate significance. 
 #-----------
 # Figure 2B
 #-----------
 # Scott's bin
-SB = hist(PPP_SUM$EC50, breaks = "Scott", xlab = "EC50", main = "Scott's bin of Mean EC50 values")
+SB = hist(Comb_Summary$Mean_EC50, breaks = "Scott", xlab = "EC50", main = "Scott's bin of Mean EC50 values")
 SB_Breaks = SB$breaks
 SB_df <- data.frame(x = SB$mids, freq = SB$counts)
 SB_Final = ggplot(data = SB_df, aes(x = x, y = freq)) +
-  geom_bar(stat = "identity") + theme_classic()+
+  geom_bar(stat = "identity") + theme_few()+
   scale_x_continuous(breaks = SB_Breaks) +
-  labs(y = "Frequency", x= expression(EC[50]))+
+  labs(y = "Frequency", x= expression(EC[50] ~ paste("(",mu, "g/ml)")))+
   theme(axis.text.x = element_text(size = 8),
         axis.text.y = element_text(size = 8),
-        axis.title=element_text(size=10,face="bold"))
+        axis.title=element_text(size=10))
 SB_Final
 #ggsave("PPP_EC50_Scotts_Bin.pdf",SB_Final, width=6, height=4, units="in")
-
-
+ggsave("PPP_EC50_Scotts_Bin_Paper.v4.pdf",SB_Final, width=4.28, height=3.78, units="in")
